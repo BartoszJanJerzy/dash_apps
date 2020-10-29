ada39697703254a63d16354f8e08d39e5cee150f
BartoszJanJerzy <67953614+BartoszJanJerzy@users.noreply.github.com>
2020-10-29 16:49:42 +0100

    style's module

---
+++ PizzaChoice/style.py
@@ -0,0 +1,68 @@
+style_link = {
+    'font-size':12,
+    'color':'#292e29',
+    'text-decoration':'none',
+
+    'border':'1px solid #9aab9a',
+    'border-radius':5,
+    'padding':'10px 30px',
+    'margin':'0px 20px 0px 0px'
+}
+
+style_button = {
+    'font-size':12,
+    'color':'#292e29',
+    'background-color':'rgba(255,255,255,0)',
+    'text-decoration':'none',
+
+    'border':'1px solid #9aab9a',
+    'border-radius':5,
+    'padding':'10px 30px 10px 30px',
+    'margin':'0px 20px 0px 0px'
+}
+
+style_img = {
+    'width':1400,
+    'border-radius':40,
+    'margin':'0px auto'
+}
+
+style_loading = {
+    'border-radius': 20,
+    'width': 700,
+    'padding': 20,
+
+    'position': 'absolute',
+    'top': 50,
+    'left': 100,
+
+    'background-color': 'rgba(255,255,255,0.7)',
+}
+
+style_loading_2 = {
+    'position':'absolute',
+    'top':-800,
+    'left':400,
+}
+
+style_comment = {'font-size':20, 'font-style':'italic'}
+
+style_order = {
+    'border-radius': 20,
+
+    'width': 300,
+    'padding': 20,
+
+    'position':'absolute',
+    'top':50,
+    'right':100,
+
+    'background-color':'rgba(255,255,255,0.7)',
+    'text-align':'center'
+}
+
+style_whole = {
+    'width':1400,
+    'margin':'0px auto',
+    'position':'relative',
+}
