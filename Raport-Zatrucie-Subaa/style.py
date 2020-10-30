c2ba1da7d0f19561e084779a2a400feba363cf37
BartoszJanJerzy <67953614+BartoszJanJerzy@users.noreply.github.com>
2020-10-30 15:00:11 +0100

    style module

---
+++ Raport-Zatrucie-Subaa/style.py
@@ -0,0 +1,141 @@
+colors = {
+    'very_dark':'#0A0908',
+    'dark':'#22333B',
+    'very_light':'#EAE0D5',
+    'light':'#C6AC8F',
+    'brown':'5E503F',
+    'light_grey':'#e0e0e0'
+}
+
+style_head = {
+    'font-size':30,
+    'font-family':'Georgia',
+    'text-align':'center',
+
+    'border':'3px solid #5E503F',
+    'padding':'20px 100px 20px 100px',
+    'margin-top':10,
+    'width':'60%',
+
+    'color':colors['very_light'],
+    'background-color':colors['dark'],
+    'border-radius':'40px'
+}
+
+style_head_2 = {
+    'display':'flex',
+    'justify-content':'center',
+    'align-items':'center',
+}
+
+style_graph_choice = {
+    'font-size':25,
+    'font-family':'Georgia',
+    'color':colors['very_light'],
+}
+
+style_interpretation_h2 = {
+    'font-size':25,
+    'font-family':'Georgia',
+    'color':colors['very_light']
+}
+
+style_interpretation_h3 = {
+    'font-size':20,
+    'font-family':'Georgia',
+    'color':colors['very_light']
+}
+
+style_interpretation_cont = {
+    'border':'1px solid #C6AC8F',
+    'padding':'10px',
+    'margin-left':20,
+    'width':600,
+    'max-height':350,
+}
+
+style_options_cont = {
+    'padding':20,
+    'width':'90%',
+    'margin':'0px auto',
+
+    'display':'flex',
+    'justify-content':'center',
+    'align-items':'center',
+
+    'background-color':colors['dark'],
+    'border-radius':'40px',
+    'border':'3px solid #5E503F',
+}
+
+style_graph = {
+    'padding':'20px',
+    'width':850,
+    'margin-right':'10px',
+
+    'background-color':colors['dark'],
+    'border-radius':'40px',
+    'border':'3px solid #5E503F',
+}
+
+style_ache_table = {
+    'font-size': 14,
+    'font-family': 'Georgia',
+    'margin': '0px',
+    'color': colors['very_light']
+}
+
+style_ache_table_2 = {
+    'min-width': 400,
+    'font-size': 12,
+    'font-family': 'Georgia',
+}
+
+style_buche_table = {
+    'font-size': 14,
+    'font-family': 'Georgia',
+    'margin': '0px',
+    'color': colors['very_light']
+}
+
+style_buche_table_2 = {
+    'min-width': 400,
+    'margin': '0px',
+    'font-size': 12,
+    'font-family': 'Georgia'
+}
+
+style_tabele = {
+    'padding':'20px 0px 20px 0px',
+    'width':550,
+    'margin-left':'10px',
+
+    'display':'flex',
+    'flex-direction':'column',
+    'justify-content':'center',
+    'align-items':'center',
+
+    'background-color':colors['dark'],
+    'border-radius':'40px',
+    'border':'3px solid #5E503F',
+}
+
+style_tabele_2 = {
+    'width':'95%',
+    'margin':'0px auto',
+
+    'display':'flex',
+    'flex-direction':'row',
+    'justify-content':'center',
+     'align-items':'center',
+
+     'background-color':colors['very_dark'],
+}
+
+style_whole = {
+    'background-color':colors['very_dark'],
+    'width':'100%',
+    'height':'100%',
+    'margin':'0px',
+    'padding':'0px'
+}
